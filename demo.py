9915b88b847dd9381d9c6fec967d03e6c76c42d1
voidnologo <(none)>
2017-10-18 08:49:59 -0500

    example

--- demo.py
+++ demo.py
@@ -2,6 +2,9 @@ import asyncio
 from prime import is_prime, print_matches, repetitive_message, fib, search, lucas
 
 
+# g = async_search(lucas(), lambda x: len(str(x)) >= 6)
+
+
 scheduler = asyncio.get_event_loop()
 a = print_matches(lucas(), is_prime)
 b = repetitive_message('This is a rep message', 1)
